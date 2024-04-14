1b40c36c43e0fb84657f19709bda2e0a32b7699a
ArchBase <archbase557@gmail.com>
2024-04-14 20:12:05 +0530

    changed to no input layer architecture

--- nn/neural_network.py
+++ nn/neural_network.py
@@ -1,55 +1,55 @@
+from typing import List
+
+
 class Neuron:
     def __init__(self, no_of_weights) -> None:
         self.weights = []
-        self.bias = 0.0
-        self.buffer = 0
-        for _ in range(no_of_weights):
-            self.weights.append(0.0)
+        self.bias = 0
+        self.buffer = []
+    
     def forward_pass(self, input_data=[]):
-        self.buffer = 0
+        self.buffer.clear()
         if len(input_data) != len(self.weights):
             print("Shape error")
-            return None
+            return
         else:
             for _ in range(len(input_data)):
                 self.buffer += self.weights[_] * input_data[_]
             self.buffer += self.bias
-            self.buffer = max(0, self.buffer)
-            return self.buffer
+            return max(0, self.buffer)
+    
+
 
 
 class Dense_Layer:
-    def __init__(self, no_of_neurons, no_of_neurons_previous) -> None:
-        self.no_of_neurons_previous = no_of_neurons_previous
+    def __init__(self, no_of_neurons=0) -> None:
         self.no_of_neurons = no_of_neurons
-        self.neurons = []
+        self.neurons: List[Dense_Layer] = []
         self.buffer = []
+    def build(self, no_of_weight_in_each_neuron=0):
         for _ in range(self.no_of_neurons):
-            self.neurons.append(Neuron(no_of_weights=self.no_of_neurons_previous))
+            self.neurons.append(Neuron(no_of_weights=no_of_weight_in_each_neuron))
+
     def forward_pass(self, input_data=[]):
         self.buffer.clear()
         for _ in range(self.no_of_neurons):
             self.buffer.append(self.neurons[_].forward_pass(input_data=input_data))
-        
-         
-
-
-class Input_Layer(Dense_Layer):
-    def __init__(self, no_of_neurons) -> None:
-        super().__init__(no_of_neurons=no_of_neurons, no_of_neurons_previous=1)
-    def forward_pass(self, input_data=[]):
-        self.buffer.clear()
-        for _ in range(len(input_data)):
-            self.buffer.append(self.neurons[_].forward_pass(input_data[_]))
-
-        return self.buffer
 
 
 class Sequential_Neural_Network:
     def __init__(self) -> None:
-        self.layers = []
-    def add(self, no_of_neurons):
-        if not self.layers:
-            self.layers.append(Input_Layer(no_of_neurons=no_of_neurons))
-        else:
-            self.layers.append(Dense_Layer(no_of_neurons=no_of_neurons, no_of_neurons_previous=self.layers[-1].no_of_neurons))
+        self.layers: List[Dense_Layer] = []
+
+    def add(self, layer=Dense_Layer()):
+        self.layers.append(layer)
+    def build(self, input_length=0):
+        self.layers[0].build(input_length)
+
+        for _ in range(1, len(self.layers)):
+            self.layers[_].build(self.layers[_ - 1].no_of_neurons)
+
+    def forward_pass(self, input_data=[]):
+
+
+
+
